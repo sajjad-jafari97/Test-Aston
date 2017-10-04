62a88e463cc271f608085db62f13b6a3394735f0
sajjad <sajjadjafari081@gmail.com>
2017-10-04 14:02:52 +0200

    one page

---
+++ One Page/assets/js/app.js
@@ -0,0 +1,11 @@
+var log = function log(data){
+   "use strict";
+   window.console.log(data);
+};var app = (function app(){
+   "use strict";
+   
+   window.onload = function start(){
+     console.log('ready!');
+       scroll();
+   };
+}());
